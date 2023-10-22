93ff7e5da1299239132b3d716e6cb6ad735c155d
KafuiAdaku <dennisadaku@ymail.com>
2023-10-22 02:14:06 -0700

    task2: pycodestyle correction

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application
-    route: /c/<text>: display “C ” followed by the value of the text 
+    route: /c/<text>: display “C ” followed by the value of the text
     variable (replace underscore _ symbols with a space )
     Your web application must be listening on `0.0.0.0`, port `5000`"""
 from flask import Flask
@@ -20,11 +20,13 @@ def hbnb():
     """A function that defines what happens when `/hbnb` is queried"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """A function that defines what happens when `/c/<text>` is queried"""
     mod_txt = text.replace("_", " ")
     return "C {}".format(mod_txt)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
