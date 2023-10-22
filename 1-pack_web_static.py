b9e60f8ceb54c001d4a3f1fbdc3baf8cfa59cf8e
KafuiAdaku <dennisadaku@ymail.com>
2023-10-22 01:23:30 -0700

    pushed modified files 'deploy web_static'

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """a Fabric script that generates a .tgz archive from the contents of the
 `web_static` folder of your AirBnB Clone repo, using the function `do_pack`
 """
-from fabric.api import local
+from fabric.api import *
 from datetime import datetime
 
 
